86d0d34d4dbaeb54dcec75bb444e3324940cef99
Robert Adams <misterblue@misterblue.com>
2016-03-27 21:31:49 -0700

    Initial source skeleton of AssetTest.

---
+++ AssetTest/AssetTestModule.cs
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 Robert Adams
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Reflection;
+
+using log4net;
+using Mono.Addins;
+using Nini.Config;
+
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+
+[assembly: Addin("Basil_AssetTest", "1.0")]
+[assembly: AddinDependency("OpenSim", "0.8.1")]
+
+namespace Basil.AssetTest
+{
+    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "Basil_AssetTest")]
+    public class AssetTest : INonSharedRegionModule
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static String LogHeader = "[Basil.AssetTest]";
+        
+        // IRegionModuleBase.Name()
+        public string Name { get { return "Basil.AssetTest"; } }        
+        
+        // IRegionModuleBase.ReplaceableInterface()
+        public Type ReplaceableInterface { get { return null; } }
+        
+        // IRegionModuleBase.ReplaceableInterface()
+        public void Initialise(IConfigSource source)
+        {
+            m_log.DebugFormat("{0}: INITIALIZED MODULE", LogHeader);
+        }
+        
+        // IRegionModuleBase.Close()
+        public void Close()
+        {
+            m_log.DebugFormat("{0}: CLOSED MODULE", LogHeader);
+        }
+        
+        // IRegionModuleBase.AddRegion()
+        public void AddRegion(Scene scene)
+        {
+            m_log.DebugFormat("{0}: REGION {1} ADDED", LogHeader, scene.RegionInfo.RegionName);
+        }
+        
+        // IRegionModuleBase.RemoveRegion()
+        public void RemoveRegion(Scene scene)
+        {
+            m_log.DebugFormat("{0}: REGION {1} REMOVED", LogHeader, scene.RegionInfo.RegionName);
+        }        
+        
+        // IRegionModuleBase.RegionLoaded()
+        public void RegionLoaded(Scene scene)
+        {
+            m_log.DebugFormat("{0}: REGION {1} LOADED", LogHeader, scene.RegionInfo.RegionName);
+        }                
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# BasilOS
+
+[OpenSimulator] region module for generating content for a [Basil viewer].
+This is early code development so things are being tried out.
+Eventually there will be explanatory and configuration information here.
+
+[OpenSimulator]: http://opensimulator.org/
+[Basil viewer]: http://blog.misterblue.com/basil/
+
+
+
