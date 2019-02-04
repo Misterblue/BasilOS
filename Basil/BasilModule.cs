b02307fc77bcb4be850a19f43c76cd7039d7917a
Robert Adams <misterblue@misterblue.com>
2019-02-03 16:08:35 -0800

    Update target framework to 4.6 (from 4.0). Fix some typos and style stuff.

--- Basil/BasilModule.cs
+++ Basil/BasilModule.cs
@@ -15,10 +15,6 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Reflection;
